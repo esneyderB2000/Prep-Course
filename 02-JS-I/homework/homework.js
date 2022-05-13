f06282e12de40b72a28436b17974e66968e6680c
Esneyder <brinezgomezesneyder20@gmail.com>
2022-05-12 21:24:52 -0500

    resolviendo ejercicios en calse

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -242,12 +242,14 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  if (letra.length > 1) return 'dato'
-  if (letra === 'a' || letra === 'e' || letra === 'i' || letra 'o' || letra 'u'){
+  if (letra.length > 1) return 'dato';
+  if (letra === 'a' || letra === 'e' || letra === 'i' || letra === 'o' || letra === 'u')
+  {
     return 'es vocal'
   }
   else {
     return 'dato incorrecto'
+}
 }
 
 
