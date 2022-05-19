6431a72bdae8d9113dc22bae9ae37499d32745a9
Esneyder <brinezgomezesneyder20@gmail.com>
2022-05-18 20:12:17 -0500

    ejercicio JSIV resuelto

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -158,7 +158,6 @@ function agregarMetodoCalculoDescuento(producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
-  
   producto.calcularPrecioDescuento = function(){
     return  this.precio - (this.precio * this.porcentajeDeDescuento);
   }
